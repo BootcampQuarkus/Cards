83608050e86bbac2eba57b0caaeb9dd4c41bb4a3
Pedro <pdiazstam@gmail.com>
2023-02-16 15:02:29 -0500

    Changes

--- ms-cards/src/main/resources/import.sql
+++ ms-cards/src/main/resources/import.sql
@@ -2,6 +2,69 @@
 -- The commands are commented as their support depends of the database
 insert into cardtype (id, name, createdat) values(nextval('hibernate_sequence'), 'Credit','2023.01.01');
 insert into cardtype (id, name, createdat) values(nextval('hibernate_sequence'), 'Debit','2023.01.01');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+
+
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 323,  1, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 323,  2, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 323,  3, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324,  4, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324,  5, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324,  6, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324,  7, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324,  8, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324,  9, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 10, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 11, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 12, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 13, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 14, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 15, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 16, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 17, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 18, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 19, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 20, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 21, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 22, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 23, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 24, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 25, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 26, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 27, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 28, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 29, 1, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 30, 1, '2023.01.01');
+
+
+
+
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 323, 61, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 323, 62, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 323, 63, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324, 64, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324, 65, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324, 66, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324, 67, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324, 68, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 324, 69, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 70, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 71, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 72, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 73, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 74, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 75, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 76, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 77, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 78, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 79, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 80, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 325, 81, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 82, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 83, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 84, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 85, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 86, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 87, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 88, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 89, 2, '2023.01.01');
+insert into card (id, serial, pin, month, year, cvv, customerId, productId, cardTypeId, createdat) values(nextval('hibernate_sequence'), '1234-1234-1234-1236','1234', 10, 2025, 123, 326, 90, 2, '2023.01.01');
